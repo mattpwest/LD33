793eae1a673882e610f0a251416a878bc15f4182
sas.vanderwest <sas.vanderwest@entelect.co.za>
2015-08-22 11:28:06 +0200

    We have units charging!

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.Cryptography;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float Speed = 1;
+    private GameObject destination;
+    private Rigidbody2D body;
+
+    public void SetDestination(GameObject destination)
+    {
+        this.destination = destination;
+    }
+
+    // Use this for initialization
+    private void Start()
+    {
+        this.body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (destination == null)
+        {
+            return;
+        }
+
+        Vector2 towards = destination.transform.position - transform.position;
+
+        body.velocity = towards;
+
+        if (transform.position == destination.transform.position)
+        {
+            destination = null;
+        }
+    }
+}
---
+++ Assets/Scripts/UnitsHandler.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class UnitsHandler : MonoBehaviour
+{
+    private List<Move> units;
+    private List<GameObject> buildings;
+    public GameObject BuildingsParent;
+
+    // Use this for initialization
+    private void Start()
+    {
+        this.buildings = GameObject.FindGameObjectsWithTag("Building").ToList();
+        this.units = this.gameObject.GetComponentsInChildren<Move>().ToList();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        foreach(var unit in this.units)
+        {
+            Vector2 position = unit.GetComponent<Rigidbody2D>().position;
+
+            unit.SetDestination(this.GetDistanceFrom(position));
+        }
+    }
+
+    private GameObject GetDistanceFrom(Vector2 position)
+    {
+        GameObject closest = null;
+        float distance = 0f;
+
+        foreach(var building in buildings)
+        {
+            float newDistance = Vector2.Distance(position, building.transform.position);
+
+            if(newDistance >= distance && distance != 0)
+            {
+                continue;
+            }
+            distance = newDistance;
+            closest = building;
+        }
+
+        return closest;
+    }
+}
