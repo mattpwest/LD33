a5a3b963e5ef0d7253562777f6d17bf43a1c8ab4
Matt Van Der Westhuizen <matt.vanderwest@entelect.co.za>
2015-08-23 13:27:58 +0200

    Village goals achieved display added.

---
+++ Assets/Scripts/UI/UpdateGoalsAchieved.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UpdateGoalsAchieved : MonoBehaviour {
+
+    public int thisGoal = 1;
+    public Sprite unachieved;
+    public Sprite achieved;
+    VillageStats villageStats;
+    Image image;
+
+	void Start () {
+        villageStats = FindObjectOfType<VillageStats>();
+        image = GetComponent<Image>();
+        image.sprite = unachieved;
+	}
+	
+	void Update () {
+        if (villageStats.GoalsAchieved >= thisGoal && image.sprite != achieved) {
+            image.sprite = achieved;
+        }
+	}
+}
---
+++ Assets/Scripts/UI/UpdatePercentDestroyed.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UpdatePercentDestroyed : MonoBehaviour {
+
+    UnityEngine.UI.Text textWidget;
+    VillageStats villageStats;
+
+	void Start () {
+        textWidget = GetComponent<UnityEngine.UI.Text>();
+        villageStats = FindObjectOfType<VillageStats>();
+	}
+	
+	void Update () {
+        textWidget.text = string.Format("{0}%", villageStats.PercentDestroyed);
+	}
+}
