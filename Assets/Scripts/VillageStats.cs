57ba22b519922fb588496ca79f90f31f99f1539f
sas.vanderwest <sas.vanderwest@entelect.co.za>
2015-08-23 07:39:42 +0200

    Created a village stats object to track total percentage of villages destroyed

---
+++ Assets/Scripts/VillageStats.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class VillageStats : MonoBehaviour
+{
+    private List<GameObject> buildings;
+    private int totalTerror;
+    private int currentTerror;
+
+    public int PercentDestroyed
+    {
+        get
+        {
+            return currentTerror / totalTerror * 100;
+        }
+    }
+
+    // Use this for initialization
+    private void Start()
+    {
+        this.buildings = GameObject.FindGameObjectsWithTag("Building").ToList();
+        this.totalTerror = this.buildings.Sum(b => b.GetComponent<TerrorSource>().terror);
+        this.currentTerror = this.totalTerror;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        buildings.Remove(null);
+        this.currentTerror = this.buildings.Sum(b => b.GetComponent<TerrorSource>().terror);
+    }
+}
