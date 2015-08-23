f782066cb94d35eccb8a760cc9a121988e588478
sas.vanderwest <sas.vanderwest@entelect.co.za>
2015-08-23 21:02:56 +0200

    Some script refactoring

--- Assets/Scripts/LevelScripts/VillageStats.cs
+++ Assets/Scripts/LevelScripts/VillageStats.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
-using System;
 using UnityEngine;
 
 public class VillageStats : MonoBehaviour
@@ -8,25 +8,57 @@ public class VillageStats : MonoBehaviour
     private List<GameObject> buildings;
     private int totalTerror;
     private int currentTerror;
-    private bool goalFiftyPercent;
-    private bool goalHundredPercent;
-    private bool goalCastle;
     private GameObject castle;
+
+    private bool GoalFiftyPercent
+    {
+        get
+        {
+            return this.PercentDestroyed >= 50;
+        }
+    }
+
+    private bool GoalHundredPercent
+    {
+        get
+        {
+            return this.PercentDestroyed >= 100;
+        }
+    }
+
+    private bool GoalCastle
+    {
+        get
+        {
+            return this.castle == null;
+        }
+    }
 
     public int PercentDestroyed
     {
         get
         {
-            return (int) Math.Round(currentTerror / (float) totalTerror * 100);
+            return (int)Math.Round(this.currentTerror / (float)this.totalTerror * 100);
         }
     }
 
-    public int GoalsAchieved {
-        get {
+    public int GoalsAchieved
+    {
+        get
+        {
             int count = 0;
-            if (goalFiftyPercent) count++;
-            if (goalHundredPercent) count++;
-            if (goalCastle) count++;
+            if(this.GoalFiftyPercent)
+            {
+                count++;
+            }
+            if(this.GoalHundredPercent)
+            {
+                count++;
+            }
+            if(this.GoalCastle)
+            {
+                count++;
+            }
             return count;
         }
     }
@@ -37,26 +69,11 @@ public class VillageStats : MonoBehaviour
         this.buildings = GameObject.FindGameObjectsWithTag("Building").ToList();
         this.totalTerror = this.buildings.Sum(b => b.GetComponent<TerrorSource>().terror);
         this.currentTerror = 0;
-        this.goalFiftyPercent = false;
-        this.goalHundredPercent = false;
-        this.goalCastle = false;
     }
 
     private void Update()
     {
-        buildings.Where(b => b == null).ToList().ForEach(b => buildings.Remove(null));
+        this.buildings.Where(b => b == null).ToList().ForEach(b => this.buildings.Remove(null));
         this.currentTerror = this.totalTerror - this.buildings.Sum(b => b.GetComponent<TerrorSource>().terror);
-
-        if (this.PercentDestroyed >= 50) {
-            goalFiftyPercent = true;
-        }
-
-        if (this.PercentDestroyed >= 100) {
-            goalHundredPercent = true;
-        }
-
-        if (castle == null) {
-            goalCastle = true;
-        }
     }
 }
