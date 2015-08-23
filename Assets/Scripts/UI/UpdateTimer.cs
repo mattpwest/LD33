a1c9db62a97fa46d2316dd268469b6628d3e98b9
Matt Van Der Westhuizen <matt.vanderwest@entelect.co.za>
2015-08-23 17:07:56 +0200

    Added timer UI.

---
+++ Assets/Scripts/UI/UpdateTimer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class UpdateTimer : MonoBehaviour {
+    UnityEngine.UI.Text textWidget;
+    Clock clock;
+
+    void Start() {
+        textWidget = GetComponent<UnityEngine.UI.Text>();
+        clock = FindObjectOfType<Clock>();
+    }
+
+    void Update() {
+        int totalRemaining = (int) Math.Round(clock.TimeLeft);
+        int minutes = totalRemaining / 60;
+        int seconds = totalRemaining % 60;
+        textWidget.text = string.Format("{0}:{1:00}", minutes, seconds);
+    }
+}
