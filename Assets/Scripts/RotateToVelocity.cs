c3b651eedeff6a9e3878fb2e76532fbd865f09ff
Matt Van Der Westhuizen <matt.vanderwest@entelect.co.za>
2015-08-24 17:59:34 +0200

    Added rotate to velocity for arrows and monsters (not perfect, but good enough).

---
+++ Assets/Scripts/RotateToVelocity.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateToVelocity : MonoBehaviour {
+
+	Rigidbody2D body;
+
+	void Start () {
+		body = GetComponent<Rigidbody2D>();
+        UpdateRotation();
+	}
+	
+	void Update () {
+        UpdateRotation();
+	}
+
+	public void UpdateRotation() {
+        // Prevent defaulting to no rotation when stopped
+        if (body.velocity.magnitude < 0.01f) {
+            return;
+        }
+
+        var direction = ((body.position + body.velocity) - body.position);
+		var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+		body.rotation = angle;
+	}
+}
